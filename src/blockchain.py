098ed011c901840577f594f56d67048074af4e39
sylvainbl <sylvain.abbg@gmail.com>
2021-07-26 23:22:57 +0200

    added proofs

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,6 +1,8 @@
 import hashlib
 import json
 from time import time
+from hashlib import sha256
+from uuid import uuid4
 
 # this class contain the blockchain and the method necessary to make it work
 
@@ -10,8 +12,8 @@ class Blockchain(object):
         self.chain = []
         self.current_transactions = []
 
-    # Create the first block of the blockchain
-    self.new_block(previous_hash=1, proof=100)
+        # Create the first block of the blockchain
+        self.new_block(previous_hash=1, proof=100)
 
     # create a new block and add it to the chain
     def new_block(self):
@@ -39,6 +41,8 @@ class Blockchain(object):
     def last_block(self):
         return self.chain[-1]
 
+    # CREATE NEW
+
     # create a new transaction that will be in the next mined block
     # params:
     # sender: name/address of the sender [str]
@@ -61,7 +65,6 @@ class Blockchain(object):
     # proof: the proof given by the proof of work algorithm [int]
     # previous_hash: the hash of the previous block (if there was one) [str]
     # return: new block [dict]
-
     def new_block(self, proof, previous_hash=None):
         block = {
             'index': len(self.chain) + 1,
@@ -76,3 +79,31 @@ class Blockchain(object):
 
         self.chain.append(block)
         return block
+
+    # PROOF
+
+    # basic proof of work, destined to change to something more spicy FIXME
+    # params:
+    # self: the blockchain
+    # last_proof: the last proof found [int]
+    # return:
+    # the new proof [int]
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    # validates the proofby doing the hash of last_proof and proof (here it need to contain 4 leading zeroes) it'll change in the future FIXME
+    # params:
+    # last_proof: the last proof found [int]
+    # param proof: the current proof we need to check [int]
+    # return:
+    # a boolean with true or flase if the hash is good or not
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
